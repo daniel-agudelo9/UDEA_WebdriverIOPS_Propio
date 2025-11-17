7f17988f3dcf6e1343f0b9bccc3014ab8f7c9e43
daniel-agudelo9 <70704227+daniel-agudelo9@users.noreply.github.com>
2025-11-17 14:18:04 -0500

    Add transfer functionality to step definitions

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,9 +1,11 @@
 import { Given, When, Then } from "@wdio/cucumber-framework";
 
 import LoginPage from '../pageobjects/login.page.js';
+import TransferPage from '../pageobjects/transfer.page.js';
 
 const pages = {
   login: LoginPage,
+  transfer: TransferPage
 };
 
 Given(/^I am on the (\w+) page$/, async (page) => {
@@ -24,5 +26,25 @@ Then(/^I should see a text saying (.*)$/, async (message) => {
     // valid username or password
     await expect($('.title')).toBeExisting();
     await expect($('.title')).toHaveTextContaining(message);
+  }
+});
+
+//TRANSFER
+When(/^I write the (.*) to transfer from the account (.*) to the account (.*) and press transfer$/,
+     async (amount, fromAccount, toAccount) => {
+       await TransferPage.transfer(amount, fromAccount, toAccount);
+});
+
+Then(/^I see (.*)$/, async (message) => {
+  if (message == "Error!") {
+    // invalid username or password
+    await expect($("//h1[normalize-space()='Error!']")).toBeExisting();
+    await expect($("//h1[normalize-space()='Error!']")).toHaveTextContaining(message);
+    await TransferPage.logout();
+  } else {
+    // valid username or password
+    await expect($("//h1[normalize-space()='Transfer Complete!']")).toBeExisting();
+    await expect($("//h1[normalize-space()='Transfer Complete!']")).toHaveTextContaining(message);
+    await TransferPage.logout();
   }
 });
